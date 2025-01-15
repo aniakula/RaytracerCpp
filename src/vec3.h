5b2c52a928f9978d8faaedb4706f42e0395af06c
Ani Akula <aniakula92@gmail.com>
2025-01-14 19:24:55 -0500

    added initial vec3 class

---
+++ src/vec3.h
@@ -0,0 +1,15 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+class vec3{
+    public:
+        double e[];
+    
+    vec3() : e{0,0,0}{};
+    vec3(double x, double y, double z) : e {x, y, z}{};
+
+
+};
+
+
+#endif
